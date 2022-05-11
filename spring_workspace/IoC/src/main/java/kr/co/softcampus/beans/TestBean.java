46d7b79a966410434e6b5ef9e7f150aa9e79b6f2
Domino419 <96913280+Domino419@users.noreply.github.com>
2022-05-12 00:19:57 +0900

    IoC(Inversion of Control) 실습

---
+++ spring_workspace/IoC/src/main/java/kr/co/softcampus/beans/TestBean.java
@@ -0,0 +1,10 @@
+package kr.co.softcampus.beans;
+
+
+ // 아무것도 구현 안 할 꺼임. 객체가 잘 생성되었는지 주소값을 찍어보는 정도로 사용 할 예정 
+public class TestBean {
+	public TestBean() {
+		System.out.println("TestBean의 생성자!");
+	}
+
+}
